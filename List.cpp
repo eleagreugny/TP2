73c6f7624e0c91451b33046498090fdb9694853e
Maria Prieto <maria.prieto-gonzalez@insa-lyon.fr>
2016-01-15 08:52:39 +0100

    Modif pushBack fait attention au nbe d'elmts dans la liste, et actualise le nombre d'elmts

--- List.cpp
+++ List.cpp
@@ -20,11 +20,19 @@ void List::Insert(const Node& nIns, const Node& nCur){
 void List::PushBack(Vector& V){
 	Node* ptrnode = new Node(V);
 	Node* cur = head_;
-	while((*cur).get_next() != nullptr){
-		cur = (*cur).get_next();
-	}
-	(*cur).set_next(ptrnode) ;
+  if (nb_elts_ >1){ //vérifie l'element suivant n'est pas nul
+	  while((*cur).get_next() != nullptr){
+		  cur = (*cur).get_next();
+	  }
+	  (*cur).set_next(ptrnode) ;
+   
+  }
+  else{ // si on a juste un elmt dans la liste, on l'ajout directement
+    (*cur).set_next(ptrnode) ;
+  }
+  nb_elts_+=1;
 }
+    
 
 void List::PopBack(){
 	Node* ptrcur = head_; //pointe sur le suivant
@@ -33,4 +41,4 @@ void List::PopBack(){
 		cur = ptrcur ;
 		ptrcur = (*ptrcur).get_next();
 	}
-	
+}
